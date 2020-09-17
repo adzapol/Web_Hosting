c13b66a095c7b3d9dfeca40865f84bc187daca8c
adzapol <61521881+adzapol@users.noreply.github.com>
2020-09-17 09:55:27 +0300

    ver 0.3.1

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,16 @@ $(document).ready(function(){
         slidesToScroll: 2
     });
 });
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        const blockID = anchor.getAttribute('href');
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
